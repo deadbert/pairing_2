0ed59accf26408ec57c3444ef07beab9c96682dd
Nick Spencer <strangebrew202@gmail.com>
2023-06-21 18:18:57 -0600

    Add places Array and .each method call that puts places

--- friends.rb
+++ friends.rb
@@ -2,3 +2,9 @@ arrays = ["sam", "kelly", "john", "robby", "mike"]
 puts arrays[2]
 puts arrays[-1]
 puts arrays[0]
+
+places = ["Arizona", "Colorado", "Miami", "New York", "Japan"]
+
+places.each do |place|
+    puts "Hello I'm here in #{place}"
+end
